67302e71bfa95795844c8b809a3f227767fd822e
an1co <skye.anico@gmail.com>
2024-04-16 20:57:13 -0400

    updated spacing in card

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
       </h2>
       {/* card */}
       <div
-        className=" flex-auto w-[392px] h-[272px] rounded-[20px]"
+        className=" flex-auto w-[392px] h-[272px] rounded-[20px] shadow-lg"
         style={{ backgroundImage: `url(${BG})` }}
       >
         <div className="mx-auto">
-          <div className="flex justify-between mx-4 pt-4 pb-6">
+          <div className="flex justify-between mx-6 pt-6 pb-6">
             <img src={Logo} alt="logo" />
             <span className=" text-base bg-white rounded-full py-1 px-3">
               Virtual
@@ -24,28 +24,34 @@ function App() {
           <span className="text-white text-[26px] leading-8 font-medium ml-6">
             <span className="font-bold mr-3">· · · ·</span> 1732
           </span>
-          <div className="flex flex-row gap-8 ml-6">
+          <div className="flex flex-row gap-10 ml-6 mt-6 pb-12">
             <div className="col-span-2">
               <p className="text-xs text-[#D3D2DE]">VALID THRU</p>
               <p className="text-white text-lg leading-6">8/28</p>
             </div>
             <div className="col-span-2">
               <p className="text-xs text-[#D3D2DE]">CVC</p>
-              <span className="text-white font-bold">· · ·</span>
+              <span className="text-white text-[26px] leading-3 font-bold">
+                · · ·
+              </span>
             </div>
           </div>
-          <div className="flex justify-between mx-5 pt-[100px]">
-            <p className="text-white text-lg leading-6 font-normal">
-              ZIP 66062
-            </p>
+          <div className="flex justify-between mx-6">
+            <p className="text-white text-lg leading-6">ZIP 66062</p>
             <img src={Visa} alt="Visa" />
           </div>
         </div>
       </div>
       {/* checkbox */}
-      <div>
-        <input type="checkbox" name="details" />
-        <label for="details">Show details</label>
+      <div className="flex row gap-3 mt-4 items-baseline">
+        <input
+          type="checkbox"
+          name="details"
+          className=" rounded w-6 h-6 checked:bg-[#ED0E61]"
+        />
+        <label for="details" className="text-base">
+          Show details
+        </label>
       </div>
     </div>
   );
