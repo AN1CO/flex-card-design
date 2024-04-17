57764dac01fb5b83b23565a8d7975c6ab999aa5c
an1co <skye.anico@gmail.com>
2024-04-17 16:35:23 -0400

    updated responsiveness

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 function App() {
   const [checked, setChecked] = useState(false);
   return (
-    <div className="container flex-auto px-2 pt-8 mx-[55px]">
+    <div className="container px-2 pt-8 max-sm:mx-auto mx-[55px]">
       <h1 className="text-[26px] leading-8 font-medium">Flex card</h1>
       <h2 className="text-xl leading-8 font-normal mb-[31px]">
         Used for pre-tax purchases
