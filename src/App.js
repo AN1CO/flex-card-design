ce4f7a2f45b2f7d89e143fe8b2b5a2d8f3e1fe0a
an1co <skye.anico@gmail.com>
2024-04-16 20:20:49 -0400

    styling card, midway

--- src/App.js
+++ src/App.js
@@ -4,30 +4,46 @@ import BG from "./assets/card_bg.svg";
 
 function App() {
   return (
-    <div className="container mx-auto px-1 py-2">
-      <h1 className="text-2xl/[26px] leading-8 font-medium">Flex card</h1>
+    <div className="container flex-auto px-2 pt-8 mx-5">
+      <h1 className="text-[26px] leading-8 font-medium">Flex card</h1>
       <h2 className="text-xl leading-8 font-normal mb-6">
         Used for pre-tax purchases
       </h2>
       {/* card */}
       <div
-        className="w-[392px] h-[272px] rounded-[20px]"
+        className=" flex-auto w-[392px] h-[272px] rounded-[20px]"
         style={{ backgroundImage: `url(${BG})` }}
       >
         <div className="mx-auto">
-          <img src={Logo} alt="logo" />
-          <span className=" text-base bg-white rounded-full py-1 px-3">
-            Virtual
+          <div className="flex justify-between mx-4 pt-4 pb-6">
+            <img src={Logo} alt="logo" />
+            <span className=" text-base bg-white rounded-full py-1 px-3">
+              Virtual
+            </span>
+          </div>
+          <span className="text-white text-[26px] leading-8 font-medium ml-6">
+            <span className="font-bold mr-3">· · · ·</span> 1732
           </span>
-          <div>
-            <p className="text-white">VALID THRU</p>
-            <p>···</p>
-            <p>CVC</p>
+          <div className="flex flex-row gap-8 ml-6">
+            <div className="col-span-2">
+              <p className="text-xs text-[#D3D2DE]">VALID THRU</p>
+              <p className="text-white text-lg leading-6">8/28</p>
+            </div>
+            <div className="col-span-2">
+              <p className="text-xs text-[#D3D2DE]">CVC</p>
+              <span className="text-white font-bold">· · ·</span>
+            </div>
+          </div>
+          <div className="flex justify-between mx-5 pt-[100px]">
+            <p className="text-white text-lg leading-6 font-normal">
+              ZIP 66062
+            </p>
+            <img src={Visa} alt="Visa" />
           </div>
         </div>
       </div>
       {/* checkbox */}
-      <div class="">
+      <div>
         <input type="checkbox" name="details" />
         <label for="details">Show details</label>
       </div>
